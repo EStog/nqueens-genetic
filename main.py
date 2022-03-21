686567026ed4d8d7aacf5bf4d901553934d67887
Ernesto Soto Gómez <esto.yinyang@gmail.com>
2022-03-21 16:12:24 -0400

    update main.py

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
 import multiprocessing
+import time
 
 import deap.base
 import deap.creator
 import deap.tools
-import matplotlib.pyplot as plt
 import pandas
 
-from variant_ga_optimized import variant_ag_optimized
+from variant_ga_binary_simple import variant_ag_binary_simple
 from variant_ga_integer_simple import variant_ag_integer_simple
+from variant_ga_optimized import variant_ag_optimized
 
-MIN_QUEENS_AMOUNT = 100
-MAX_QUEENS_AMOUNT = 100
-QUEENS_AMOUNT_STEP = 100
-POPULATION_SIZE = 400
+MIN_INDEX = 2
+MAX_INDEX = 7
+
+MIN_QUEENS_AMOUNT = 2**MIN_INDEX
+MAX_QUEENS_AMOUNT = 2**MAX_INDEX
+
+QUEENS_AMOUNTS = [2**i for i in range(MIN_INDEX, MAX_INDEX+1)]
+
+POPULATION_SIZE = 300
 GENERATION_NUMBER = 200
 
 CROSSOVER_PROBABILITY = 0.9
 MUTATION_PROBABILITY = 0.1
 
-RANDOM_SEED = 1
+RANDOM_SEED = 1500
+
+VARIANTS = [variant_ag_binary_simple, variant_ag_integer_simple, variant_ag_optimized]
+VARIANTS_NAMES = ['Simple Bin.', 'Simple Int.', 'Optimized']
+VARIANTS_AMOUNT = len(VARIANTS)
 
 
 def main():
@@ -29,38 +39,48 @@ def main():
     pool = multiprocessing.Pool()
     toolbox.register("map", pool.map)
 
-    bests1 = []
-    bests2 = []
+    bests = {name: [] for name in VARIANTS_NAMES}
+    times = {name: [] for name in VARIANTS_NAMES}
+    logbook = {name: None for name in VARIANTS_NAMES}
 
-    for i in range(MIN_QUEENS_AMOUNT, MAX_QUEENS_AMOUNT+1, QUEENS_AMOUNT_STEP):
-        print(f'\nComputing for {i} queens...')
-        _, b1, logbook1 = variant_ag_integer_simple(
-            i, POPULATION_SIZE, CROSSOVER_PROBABILITY,
-            MUTATION_PROBABILITY, GENERATION_NUMBER, toolbox, verbose=False)
-        _, b2, logbook2 = variant_ag_optimized(
-            i, POPULATION_SIZE, CROSSOVER_PROBABILITY,
-            MUTATION_PROBABILITY, GENERATION_NUMBER, toolbox, verbose=False)
-        bests1.append(b1.fitness.values[0])
-        bests2.append(b2.fitness.values[0])
+    for i in QUEENS_AMOUNTS:
+        for variant, name in zip(VARIANTS, VARIANTS_NAMES):
+            print(f'\nComputing {name} for {i} queens...')
+            init = time.time()
+            _, b, logbook[name] = variant(
+                i, POPULATION_SIZE, CROSSOVER_PROBABILITY,
+                MUTATION_PROBABILITY, GENERATION_NUMBER, toolbox,
+                verbose=False)
+            times[name].append(time.time() - init)
+            bests[name].append(b.fitness.values[0])
 
-    d = pandas.DataFrame(zip(bests1, bests2), index=range(
-        MIN_QUEENS_AMOUNT, MAX_QUEENS_AMOUNT+1, QUEENS_AMOUNT_STEP), columns=['Simple', 'Optimized'])
+    d = pandas.DataFrame(bests, index=QUEENS_AMOUNTS)
     d.to_csv('bests.csv')
-    d.plot(title='Bests fitness values per problem size',
-           xlabel='Queens amount',
-           ylabel='Fitness')
-    plt.savefig('bests.svg')
+    fig = d.plot(title='Bests fitness values per problem size',
+                 xlabel='Queens amount',
+                 ylabel='Fitness').get_figure()
+    fig.savefig('bests.svg')
 
-    minFitnessValues1, meanFitnessValues1 = logbook1.select("min", "avg")
-    minFitnessValues2, meanFitnessValues2 = logbook2.select("min", "avg")
-    d = pandas.DataFrame(zip(minFitnessValues1, meanFitnessValues1, minFitnessValues2, meanFitnessValues2), columns=[
-                         'Simple V. Min', 'Simple V. Mean', 'Optimized V. Min', 'Optimized V. Mean'])
+    d = pandas.DataFrame(times, index=QUEENS_AMOUNTS)
+    d.to_csv('times.csv')
+    fig = d.plot(title='Execution times per problem size',
+                 xlabel='Queens amount',
+                 ylabel='Execution time (s)').get_figure()
+    fig.savefig('times.svg')
+
+    mins_means = []
+    graphs_names = []
+    for name in VARIANTS_NAMES:
+        mins_means.extend(logbook[name].select('min', 'avg'))
+        graphs_names.extend((f'{name} min', f'{name} mean'))
+
+    d = pandas.DataFrame(zip(*mins_means), columns=graphs_names)
 
     d.to_csv('stats.csv')
-    d.plot(title=f'Min/Mean fitness per generation for {MAX_QUEENS_AMOUNT} queens',
-           xlabel='Generation number',
-           ylabel='Fitness')
-    plt.savefig('stats.svg')
+    fig = d.plot(title=f'Min/Mean fitness per generation for {MAX_QUEENS_AMOUNT} queens',
+                 xlabel='Generation number',
+                 ylabel='Fitness').get_figure()
+    fig.savefig('stats.svg')
 
 
 if __name__ == "__main__":
